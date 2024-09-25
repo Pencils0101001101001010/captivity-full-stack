bce604e8e0bbc5c3e2eca99f44ebae4d957d693c
Jaco <thiartjaco518@gmail.com>
2024-09-25 12:04:30 +0200

    start up with Lucia session provider

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { validateRequest } from "@/auth";
+import { redirect } from "next/navigation";
+
+// Define the routes for each role
+const roleRoutes = {
+  USER: "/dashboard",
+  ADMIN: "/admin",
+};
+
+type UserRole = keyof typeof roleRoutes;
+
+export default async function RoleBasedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { user } = await validateRequest();
+
+  if (user) {
+    const userRole = user.role as UserRole;
+
+    if (userRole in roleRoutes) {
+      redirect(roleRoutes[userRole]);
+    } else {
+      redirect("/");
+    }
+  }
+  return <>{children}</>;
+}
