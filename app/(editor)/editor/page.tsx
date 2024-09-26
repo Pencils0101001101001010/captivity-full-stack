1a6a6d138bb63dfbcd2bd2fe19bb5a840a61843e
Jaco <thiartjaco518@gmail.com>
2024-09-27 00:17:05 +0200

    set routing

---
+++ app/(editor)/editor/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Welcome to the Editor Panel</div>;
+};
+
+export default Page;
---
+++ app/(promo)/promo/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Welcome to the Promo Panel</div>;
+};
+
+export default Page;
