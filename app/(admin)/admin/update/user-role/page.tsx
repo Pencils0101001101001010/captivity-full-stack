ea19df331b366840506fec169c61c799aaaf647f
Jaco <thiartjaco518@gmail.com>
2024-09-27 17:45:43 +0200

    set

--- app/(admin)/admin/update/user-role/page.tsx
+++ app/(admin)/admin/update/user-role/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Newly Registered User</div>;
+  return (
+    <div>
+      <h1>Newly Registered User</h1>
+    </div>
+  );
 };
 
 export default Page;
