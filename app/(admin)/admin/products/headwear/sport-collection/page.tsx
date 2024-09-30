004afb8746c935cd1862f32353d5446822b007ee
Jaco <thiartjaco518@gmail.com>
2024-09-30 12:59:53 +0200

    set sports collection table

---
+++ app/(admin)/admin/products/headwear/sport-collection/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SportTable from "./SportTable";
+
+const Page = () => {
+  return (
+    <div>
+      <SportTable />
+    </div>
+  );
+};
+
+export default Page;
