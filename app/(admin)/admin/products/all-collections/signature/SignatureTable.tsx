548d111c1ac87d444ce3e09ec3ecf0e0f0cb29d4
Jaco <thiartjaco518@gmail.com>
2024-10-01 14:00:17 +0200

    set update

---
+++ app/(admin)/admin/products/all-collections/signature/SignatureTable.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignatureTable = () => {
+  return <div>Signature Table</div>;
+};
+
+export default SignatureTable;
---
+++ app/(admin)/admin/products/all-collections/signature/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignatureTable from "./SignatureTable";
+
+const Page = () => {
+  return (
+    <div>
+      <SignatureTable />
+    </div>
+  );
+};
+
+export default Page;
