474d13938f6c60e28ee8865920c163c09e37d1f0
Jaco <thiartjaco518@gmail.com>
2024-10-05 00:26:34 +0200

    set cart details page setup

---
+++ app/(customer)/customer/cart/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <h1>Product Card Inside of the cart</h1>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ app/(customer)/customer/cart/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+
+const Page = () => {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
+};
+
+export default Page;
