66ee0e81f4fd2f81163b9ad46ffccbf3a1b949e9
Jaco <thiartjaco518@gmail.com>
2024-10-05 16:21:46 +0200

    set data relation model type

---
+++ lib/types.ts
@@ -0,0 +1,44 @@
+import { Prisma } from "@prisma/client";
+
+export function getUserDataSelect() {
+  return {
+    id: true,
+    username: true,
+    displayName: true,
+    avatarUrl: true,
+    bio: true,
+    role: true,
+    createdAt: true,
+  } satisfies Prisma.UserSelect;
+}
+
+export type UserData = Prisma.UserGetPayload<{
+  select: ReturnType<typeof getUserDataSelect>;
+}>;
+
+export function getProductDataInclude() {
+  return {
+    user: {
+      select: getUserDataSelect(),
+    },
+  } satisfies Prisma.ProductInclude;
+}
+
+export type ProductData = Prisma.ProductGetPayload<{
+  include: ReturnType<typeof getProductDataInclude>;
+}>;
+
+export function getCartDataInclude() {
+  return {
+    users: {
+      select: getUserDataSelect(),
+    },
+    products: {
+      include: getProductDataInclude(),
+    },
+  } satisfies Prisma.CartInclude;
+}
+
+export type CartData = Prisma.CartGetPayload<{
+  include: ReturnType<typeof getCartDataInclude>;
+}>;
