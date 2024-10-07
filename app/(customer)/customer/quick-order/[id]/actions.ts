eeb8bf989b5fff3687eb20792f5604f79b05cbab
Jaco <thiartjaco518@gmail.com>
2024-10-07 13:56:17 +0200

    set

--- app/(customer)/customer/quick-order/[id]/actions.ts
+++ app/(customer)/customer/quick-order/[id]/actions.ts
@@ -2,42 +2,37 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 import { Product } from "@prisma/client";
 
 type FetchProductByIdResult =
-  | { success: true; data: Product }
+  | { success: true; data: Product | null }
   | { success: false; error: string };
 
 export async function fetchProductById(
-  productId: number
+  id: number
 ): Promise<FetchProductByIdResult> {
   try {
-    // Validate user
+    // Validate user session
     const { user } = await validateRequest();
     if (!user) {
       throw new Error("Unauthorized. Please log in.");
     }
 
+    // Check if the user has the ADMIN role
     if (user.role !== "CUSTOMER") {
-      throw new Error("Only customers can view product details.");
+      throw new Error("Only customers can fetch product details.");
     }
 
-    // Fetch product by ID
+    // Fetch the product from the database
     const product = await prisma.product.findUnique({
-      where: { id: productId },
+      where: { id },
     });
 
-    if (!product) {
-      return {
-        success: false,
-        error: "Product not found.",
-      };
-    }
+    // Revalidate the product detail page
+    revalidatePath(`/customer/quick-order/${id}`);
 
-    return {
-      success: true,
-      data: product,
-    };
+    return { success: true, data: product };
   } catch (error) {
     console.error("Error fetching product by ID:", error);
     return {
