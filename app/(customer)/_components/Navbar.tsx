51e12831a7520e783b47707b7e9f89c9d9ea5342
Jaco <thiartjaco518@gmail.com>
2024-10-08 20:05:56 +0200

    set second attempt

--- app/(customer)/_components/Navbar.tsx
+++ app/(customer)/_components/Navbar.tsx
@@ -20,7 +20,10 @@ const Navbar = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
 
   const renderCartIcon = () => (
-    <div className="relative cursor-pointer">
+    <div
+      className="relative cursor-pointer"
+      onClick={() => setIsCartOpen(true)}
+    >
       <ShoppingCart />
       <span
         className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold"
--- app/(customer)/_components/SlideInCart.tsx
+++ app/(customer)/_components/SlideInCart.tsx
@@ -12,7 +12,7 @@ interface SlideInCartProps {
 const SlideInCart: React.FC<SlideInCartProps> = ({ isOpen, onClose }) => {
   return (
     <div
-      className={`fixed inset-y-0 right-0 w-80 bg-white shadow-lg transform ${isOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out z-50`}
+      className={`fixed inset-y-0 right-0 w-[450px] bg-white shadow-lg transform ${isOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out z-50`}
     >
       <div className="flex justify-between items-center p-4 border-b">
         <h2 className="text-lg font-semibold">Your Cart</h2>
