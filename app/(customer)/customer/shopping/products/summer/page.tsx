7afe498fd0460a45fed4ff136bdbbc459495ab8c
Jaco <thiartjaco518@gmail.com>
2024-10-14 18:43:38 +0200

    set back button

--- app/(customer)/customer/shopping/products/summer/page.tsx
+++ app/(customer)/customer/shopping/products/summer/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import SummerCollections from "./SummerCollections";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const SummerCollectionPage = () => {
   return (
     <div>
+      <Button asChild variant={"default"} className="ml-8 mt-2">
+        <Link href={"/customer/shopping/products"}>Bact to express</Link>
+      </Button>
       <h1>
         <SummerCollections />
       </h1>
