0fe8b3d48a9ed6c5457c8aa6876e5dafca08dd2b
Jaco <thiartjaco518@gmail.com>
2024-10-14 20:34:36 +0200

    set back link

--- app/(customer)/customer/shopping/products/winter/page.tsx
+++ app/(customer)/customer/shopping/products/winter/page.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+
+import LinkButton from "../_components/LinkButton";
 import WinterCollections from "./WinterCollection";
 
-const WinterCollectionPage = () => {
+const SummerCollectionPage = () => {
   return (
     <div>
+      <LinkButton
+        href="/customer/shopping/products"
+        variant="default"
+        className="custom-class"
+      >
+        Back to express
+      </LinkButton>
       <h1>
         <WinterCollections />
       </h1>
@@ -11,4 +20,4 @@ const WinterCollectionPage = () => {
   );
 };
 
-export default WinterCollectionPage;
+export default SummerCollectionPage;
