b5d3dcfa4e5eae9a980357fa9f12c3463a80a85c
Jaco <thiartjaco518@gmail.com>
2024-10-21 14:08:06 +0200

    set summer carousel route

---
+++ app/(customer)/customer/shopping/summer/SummerCarousel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SummerCarousel = () => {
+  return <div>Carousel with summer divided into categories</div>;
+};
+
+export default SummerCarousel;
---
+++ app/(customer)/customer/shopping/summer/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SummerCarousel from "./SummerCarousel";
+
+const SummerPage = () => {
+  return (
+    <div>
+      <SummerCarousel />
+    </div>
+  );
+};
+
+export default SummerPage;
