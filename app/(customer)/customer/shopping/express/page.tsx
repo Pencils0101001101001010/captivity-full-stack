8327a2918ebf5093a8e4eecca05775dd541e7e3f
Jaco <thiartjaco518@gmail.com>
2024-10-21 21:58:28 +0200

    set the collection routes => Intern duties

---
+++ app/(customer)/customer/shopping/express/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExpressLanding from "./ExpressLanding";
+
+const ExpressPage = () => {
+  return (
+    <div>
+      <ExpressLanding />
+    </div>
+  );
+};
+
+export default ExpressPage;
