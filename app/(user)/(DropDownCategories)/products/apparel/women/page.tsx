455743007431024a79c8419c23c4daf0993feb48
Siraaj <morrissiraj@gmail.com>
2024-10-22 13:37:41 +0200

    setting up the page dor womens category

---
+++ app/(user)/(DropDownCategories)/products/apparel/women/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WomensCollectionPage = () => {
+  return (
+    <div>
+      <h1>Womens Collection</h1>
+    </div>
+  )
+}
+
+export default WomensCollectionPage
