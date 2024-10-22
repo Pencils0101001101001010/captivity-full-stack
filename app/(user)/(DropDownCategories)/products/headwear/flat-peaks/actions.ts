b427b35436c3c508a7b71ab443852667aa4a184c
Heini <heindev3@gmail.com>
2024-10-22 17:18:08 +0200

    added Flat-Peaks category

---
+++ app/(user)/(DropDownCategories)/products/headwear/flat-peaks/actions.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { Product } from "@prisma/client";
+
+export type FeaturedImage = {
+  id: string;
+  thumbnail: string;
+  medium: string;
+  large: string;
+  productId: string;
+};
+
+export type ProductWithFeaturedImage = Omit<Product, "featuredImage"> & {
+  featuredImage: FeaturedImage | null;
+};
+
+type FetchFlatPeaksResult =
+  | { success: true; data: ProductWithFeaturedImage[] }
+  | { success: false; error: string };
+
+export async function fetchNewHeadwear(): Promise<FetchFlatPeaksResult> {
+  try {
+    const flatPeaks = await prisma.product.findMany({
+      where: {
+        category: {
+          hasSome: ["flat-peaks"],
+        },
+        isPublished: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        featuredImage: true,
+      },
+    });
+
+    return {
+      success: true,
+      data: flatPeaks.map(product => ({
+        ...product,
+        featuredImage: product.featuredImage || null,
+      })) as ProductWithFeaturedImage[],
+    };
+  } catch (error: any) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An unexpected error occurred";
+
+    console.error(`Error fetching new headwear: ${errorMessage}`);
+
+    return { success: false, error: errorMessage };
+  }
+}
---
+++ app/(user)/(DropDownCategories)/products/headwear/flat-peaks/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FlatPeaksProductList from "./FlatPeaks";
+
+const page = () => {
+    return <FlatPeaksProductList />;
+  };
+  
+  export default page;
+  
