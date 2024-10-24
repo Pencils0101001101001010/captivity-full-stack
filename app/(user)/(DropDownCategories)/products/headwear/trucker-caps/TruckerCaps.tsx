813223d7eff78b4bcbd39e721035809edbe2ddf5
Heini <heindev3@gmail.com>
2024-10-25 00:14:17 +0200

    added useMemo, loading spinners & pagination on truckercaps* correction

--- app/(user)/(DropDownCategories)/products/headwear/trucker-caps/TruckerCaps.tsx
+++ app/(user)/(DropDownCategories)/products/headwear/trucker-caps/TruckerCaps.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
