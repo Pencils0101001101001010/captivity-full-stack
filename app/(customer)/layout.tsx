191d95b844b03adb847314c289ffe63b99d1fc25
Jaco <thiartjaco518@gmail.com>
2024-10-24 22:18:30 +0200

    set categories removal

--- app/(customer)/layout.tsx
+++ app/(customer)/layout.tsx
@@ -2,7 +2,6 @@ import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
 import Navbar from "./_components/Navbar";
-import CategoriesDropDown from "../(user)/_components/CategoriesDropDown";
 
 export default async function CustomerLayout({
   children,
@@ -17,9 +16,7 @@ export default async function CustomerLayout({
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="bg-slate-400">
-          <CategoriesDropDown />
-        </div>
+        <div className="bg-slate-400"></div>
         <div className="flex w-full grow">
           <main className="flex-grow">{children}</main>
         </div>
