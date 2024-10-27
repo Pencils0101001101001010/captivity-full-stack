f830966fbd94264dd23f839f2e56e08f590a8228
Mat04 <stopglobalwarming622@gmail.com>
2024-10-27 13:57:40 +0000

    set Camo Collection

---
+++ app/(user)/(DropDownCategories)/products/all-collections/camo-collection/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CamoProductList from "./Camo";
+
+const page = () => {
+  return (
+    <div>
+      <CamoProductList />
+    </div>
+  );
+};
+
+export default page;
