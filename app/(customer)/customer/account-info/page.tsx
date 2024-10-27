eecd04d95dec2922ca902889a17ffea5605a8d54
Pencils0101001101001010 <155194934+Pencils0101001101001010@users.noreply.github.com>
2024-10-27 13:55:45 +0200

    Finishing touches on Account info and Order history

--- app/(customer)/customer/account-info/page.tsx
+++ app/(customer)/customer/account-info/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import AccountInfoForm from "./AccountInfoForm";
+import BackToCustomerPage from "../_components/BackToCustomerButton";
 
 export default async function AccountPage() {
   const { user, session } = await validateRequest();
@@ -14,7 +15,16 @@ export default async function AccountPage() {
 
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-bold mb-6">Account Settings</h1>
+      <div className="flex items-center justify-between mb-7">
+        <span>
+          <h1 className="text-2xl font-semibold text-gray-700  ">
+            Account Settings
+          </h1>
+        </span>
+        <span className="p-2 hover:bg-neutral-100">
+          <BackToCustomerPage />
+        </span>
+      </div>
       <Suspense fallback={<div>Loading...</div>}>
         <AccountInfoForm />
       </Suspense>
