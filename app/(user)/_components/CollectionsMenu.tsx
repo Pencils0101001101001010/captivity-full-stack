23e26284abe7f9290f612c5c34b949d8030ad46c
Mat04 <stopglobalwarming622@gmail.com>
2024-10-29 08:16:57 +0000

    fixed functionality of CollectionsMenu

--- app/(user)/_components/CollectionsMenu.tsx
+++ app/(user)/_components/CollectionsMenu.tsx
@@ -49,7 +49,7 @@ export function CollectionsMenu({ products, loading }: CollectionsMenuProps) {
           {collections.map(item => (
             <li key={item}>
               <Link
-                href={`/collections/${item.toLowerCase()}`}
+                href={`/products/all-collections/${item.toLowerCase()}-collection`}
                 className="text-gray-600 hover:text-gray-900 transition-colors duration-200"
               >
                 {item}
