f6545d726305563bc875e10401115f8d242a4aa8
Jaco <thiartjaco518@gmail.com>
2024-10-31 21:12:41 +0200

    set production build

--- app/(distributor)/distributor/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DistributorDashboard from "./DistributorPannel";
-
-const Page = () => {
-  return <div><DistributorDashboard/></div>;
-};
-
-export default Page;
--- app/(promo)/promo/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>Welcome to the Promo Panel</div>;
-};
-
-export default Page;
