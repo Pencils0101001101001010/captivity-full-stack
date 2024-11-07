7cbe8aa6d8f08045f2f0bff4bcd1c5d17110a252
Jaco <thiartjaco518@gmail.com>
2024-11-07 13:15:20 +0200

    remove unwanted image whitelists

--- next.config.mjs
+++ next.config.mjs
@@ -15,18 +15,6 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
         hostname: "*.public.blob.vercel-storage.com",
       },
     ],
