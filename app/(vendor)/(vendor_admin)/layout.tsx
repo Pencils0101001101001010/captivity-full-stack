898f8f6375869febfdc96efea56de052271ba110
Jaco <thiartjaco518@gmail.com>
2024-11-11 15:40:46 +0200

    set vendor navbar and logout

--- app/(vendor)/(vendor_admin)/layout.tsx
+++ app/(vendor)/(vendor_admin)/layout.tsx
@@ -2,6 +2,7 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
+import Navbar from "./_components/Navbar";
 
 export const dynamic = "force-dynamic";
 
@@ -17,6 +18,7 @@ export default async function Layout({
   return (
     <SessionProvider value={session}>
       <div className="flex h-screen flex-col overflow-hidden">
+        <Navbar />
         <div className="flex flex-1 overflow-hidden">
           <main className="flex-1 overflow-y-auto p-6">{children}</main>
         </div>
