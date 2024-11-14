102b9d25249ed56980717c3778efb77385d4cf26
Jaco <thiartjaco518@gmail.com>
2024-11-14 22:14:42 +0200

    set dummy vendor editor prep

--- app/(vendor)/vendor/[vendor_website]/welcome/page.tsx
+++ app/(vendor)/vendor/[vendor_website]/welcome/page.tsx
@@ -1,7 +1,18 @@
+import CustomContainer from "@/components/(how to register section on home page)/CustomContainer/CustomContainer";
+import HowToRegister from "@/components/(how to register section on home page)/Register/HowToRegister";
+import BestSeller from "@/components/BestSeller/BestSeller";
+import ProductSection from "@/components/ProductSection/ProductSection";
 import React from "react";
 
 const WelcomePage = () => {
-  return <div className="my-4">page</div>;
+  return (
+    <div className="my-4">
+      <ProductSection />
+      <BestSeller />
+      <CustomContainer />
+      <HowToRegister />
+    </div>
+  );
 };
 
 export default WelcomePage;
