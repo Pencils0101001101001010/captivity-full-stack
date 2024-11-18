b85ea37194d2b496365e62f2c81b69f8256caeb6
Jaco <thiartjaco518@gmail.com>
2024-11-18 19:19:42 +0200

    remove unused button

--- app/(vendor)/vendor/[vendor_website]/_components/Footer.tsx
+++ app/(vendor)/vendor/[vendor_website]/_components/Footer.tsx
@@ -14,13 +14,6 @@ const Footer = () => {
         <ContactUs />
         <OpeningHours />
       </div>
-      <div className="container mx-auto text-center mt-10">
-        <Link href="https://captivity.co.za/contact/">
-          <button className="bg-transparent border-2 border-slate-400 text-white hover:bg-red-700 hover:border-red-700 py-2 px-4">
-            OUR OFFICES
-          </button>
-        </Link>
-      </div>
     </footer>
   );
 };
