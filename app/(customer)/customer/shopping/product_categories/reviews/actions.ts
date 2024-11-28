b5a47f652ad4937f02c0af1100e84605abe86511
Jaco <thiartjaco518@gmail.com>
2024-11-28 12:42:14 +0200

    set

--- app/(customer)/customer/shopping/product_categories/reviews/actions.ts
+++ app/(customer)/customer/shopping/product_categories/reviews/actions.ts
@@ -2,8 +2,6 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-
-import { revalidatePath } from "next/cache";
 import { Review } from "../types";
 
 interface ReviewResponse {
